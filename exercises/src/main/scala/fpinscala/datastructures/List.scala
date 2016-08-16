ab9ecd26a8fc57763ae16374afe5d38d6caa7226
Jack Low <jack.low@rea-group.com>
2016-08-16 13:23:29 +1000

    Added flatMap

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -116,4 +116,7 @@ object List { // `List` companion object. Contains functions for creating and wo
 
   def filter[A](as: List[A])(f: A => Boolean): List[A] =
     foldRight(as, List[A]())((a, b) => if (f(a)) Cons(a, b) else b)
+
+  def flatMap[A,B](as: List[A])(f: A => List[B]): List[B] =
+    foldRight(as, List[B]())((h, t) => append(f(h), t))
 }

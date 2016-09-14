7e23132ac583d2f385711a3cbc31890af3d200e5
Jack Low <jack.low@rea-group.com>
2016-09-14 12:58:38 +1000

    continue on Option

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -58,9 +58,27 @@ object Option {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
-  def variance(xs: Seq[Double]): Option[Double] = sys.error("todo")
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = sys.error("todo")
+  def variance(xs: Seq[Double]): Option[Double] = {
+    mean(xs) flatMap { m =>
+      val doubles: Seq[Double] = xs map { x =>
+        math.pow(x - m, 2)
+      }
+      mean(doubles)
+    }
+  }
+
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    (a, b) match {
+      case (None, _) => None
+      case (_, None) => None
+      case (Some(a), Some(b)) => Some(f(a, b))
+    }
+
+  def map22[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    a flatMap (aa => b map (bb => f(aa, bb)))
+
+  def lift[A,B](f: A => B): Option[A] => Option[B] = _ map f
 
   def sequence[A](a: List[Option[A]]): Option[List[A]] = sys.error("todo")
 
